58310428c28d307272a52a7c2e1a5629ddc96c00
Marcel <numax.cz@protonmail.ch>
2024-11-09 13:02:25 +0100

    Classes

--- src/pages/api/socketio.ts
+++ src/pages/api/socketio.ts
@@ -3,83 +3,13 @@ import { NextApiRequest } from "next";
 import { NextApiResponseServerIO } from "@/types/next";
 import { Server as SocketIOServer } from "socket.io";
 import noble from "@abandonware/noble";
-
-let isScanning = false;
-
-// Funkce pro spuštění skenování
-const start_scan = (io: SocketIOServer) => {
-  if (isScanning) return;
-
-  console.log("Starting BLE scan...");
-  isScanning = true;
-
-  noble.startScanning([], true);
-  io.emit("scan_status", true);
-
-  // Automatické zastavení po 30 sekundách
-  setTimeout(() => {
-    stop_scan(io);
-  }, 10000);
-};
-
-// Funkce pro zastavení skenování
-const stop_scan = (io: SocketIOServer) => {
-  if (!isScanning) return;
-
-  console.log("Stopping BLE scan...");
-  noble.stopScanning();
-  isScanning = false;
-  io.emit("scan_status", false);
-};
+import NapicuBLEServer from "../utils/NapicuServer";
+import NapicuServer from "../utils/NapicuServer";
 
 export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
   if (!res.socket.server.io) {
-    console.log("Initializing new Socket.io server...");
-
-    const httpServer: NetServer = res.socket.server as any;
-    const io = new SocketIOServer(httpServer, {
-      path: "/api/socketio",
-    });
-
-    res.socket.server.io = io;
-
-    // Při připojení nového klienta
-    io.on("connection", (socket) => {
-      console.log("New client connected");
-
-      // Pošleme stav skenování při připojení
-      socket.emit("scan_status",  isScanning);
-
-      // Posloucháme na zprávy od klienta
-      socket.on("start_scan", () => {
-        start_scan(io);
-      });
-
-      socket.on("stop_scan", () => {
-        stop_scan(io);
-      });
-    });
-
-    // Nastavení `noble` skenování
-    noble.on("stateChange", (state) => {
-      if (state === "poweredOn") {
-        console.log("BLE adapter powered on");
-      } else {
-        console.log("BLE adapter not ready");
-        noble.stopScanning();
-      }
-    });
-
-    noble.on("discover", (peripheral) => {
-      const device = {
-        id: peripheral.id,
-        name: peripheral.advertisement.localName || "Unknown",
-      };
-
- 
-      io.emit("device", device);
-    });
+    new NapicuServer(req, res).init();
   }
-
+  //@ts-ignore
   res.end();
 };
---
+++ src/pages/utils/NapicuServer.ts
@@ -0,0 +1,110 @@
+import { Server as NetServer } from "http";
+import { NextApiRequest } from "next";
+import { NextApiResponseServerIO } from "@/types/next";
+import { DefaultEventsMap, Server as SocketIOServer } from "socket.io";
+import noble from "@abandonware/noble";
+
+
+
+export default class NapicuServer {
+  private io:  SocketIOServer<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
+
+  private is_scanning: boolean = false;
+
+  private time_id: NodeJS.Timeout | null = null;
+  
+
+
+  constructor(req: NextApiRequest, res: NextApiResponseServerIO) {
+    console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - Starting...\x1b[0m");
+
+    const httpServer: NetServer = res.socket.server as any;
+    this.io = new SocketIOServer(httpServer, {
+      path: "/api/socketio",
+    });
+
+    res.socket.server.io = this.io;
+  }
+
+  public init(): void {
+    //On client connect
+    this.io.on("connection", (socket) => {
+      console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - New client connected.\x1b[0m");
+      socket.emit("scan_status", this.is_scanning);
+
+      socket.on("start_scan", this.start_scan);
+
+      socket.on("stop_scan", this.stop_scan);
+
+      socket.on("disconnect", () => {
+        const client_count: number = this.io.sockets.sockets.size;
+        console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - Client has disconnected. Number of connected clients:\x1b[0m", client_count);
+        if (client_count === 0) {
+          console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - No clients connected.\x1b[0m");
+          this.stop_scan();
+        }
+      });
+    });
+
+ 
+
+    //On noble change
+    noble.on("stateChange", this.noble_change);
+    //On noble discover
+    noble.on("discover", this.noble_discover);
+  }
+
+  private start_scan = (): void => {
+    if (!this.is_scanning) {
+      console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - Starting BLE scan...\x1b[0m");
+      this.is_scanning = true;
+    
+      noble.startScanning([], true);
+      this.emit_scan_status();
+    
+      // Automatické zastavení po 30 sekundách
+      this.time_id = setTimeout(() => {
+        this.stop_scan();
+      }, 10000);
+    }
+  }
+
+  private stop_scan = (): void => {
+    if (this.is_scanning) {
+      console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - Stopping BLE scan...\x1b[0m");
+      noble.stopScanning();
+      this.is_scanning = false;
+      this.emit_scan_status();
+    }
+
+    if(this.time_id) clearTimeout(this.time_id);
+  }
+
+  private noble_change = (state: string): void => {
+    if (state === "poweredOn") {
+      //TODO EMIT
+      console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[34m - BLE adapter powered on.\x1b[0m");
+    } else {
+      //TODO EMIT
+      console.log("\x1b[33m[NapicuServer]\x1b[0m\x1b[31m - BLE adapter powered on.\x1b[0m");
+      noble.stopScanning();
+    }
+  }
+
+  private noble_discover = (peripheral: noble.Peripheral) => {
+    const device = {
+      id: peripheral.id,
+      name: peripheral.advertisement.localName || "Unknown",
+    };
+
+    this.io.emit("device", device);
+  }
+
+  private emit_scan_status(): void {
+    this.io.emit("scan_status", this.is_scanning);
+  }
+
+    
+
+
+}
