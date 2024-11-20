b221d96d01a2d15e0292e6258beda50c6980e080
Marcel <numax.cz@protonmail.ch>
2024-11-20 22:27:53 +0100

    Fixed bugs

--- src/pages/AliasManager/ServiceAliasManager.ts
+++ src/pages/AliasManager/ServiceAliasManager.ts
@@ -7,35 +7,31 @@ interface IServicesAliasesTable {
 }
 
 export default class NapicuServiceAliasManager {
-    private static  creat_table(): void {
-        NapicuCookies.setCookies<IServicesAliasesTable[]>("services_aliases", []);
-    }
+    private static readonly COOKIES_SERVICES_ALIASES_TABLE_NAME: string = "services_aliases";
 
     public static set_alias(key: string, value: string): void {
-        const aliases_table: IServicesAliasesTable[] = NapicuCookies.getCookies<IServicesAliasesTable[]>("services_aliases") ?? [];
-        
+        const aliases_table: IServicesAliasesTable[] = NapicuCookies.getCookies<IServicesAliasesTable[]>(NapicuServiceAliasManager.COOKIES_SERVICES_ALIASES_TABLE_NAME) ?? [];
+
         const updated_aliases_table = aliases_table.some(item => item.name === key) 
             ? aliases_table.map(item => 
                 item.name === key ? { ...item, alias: value } : item
             )  
             : [...aliases_table, { name: key, alias: value }];  
 
-        NapicuCookies.setCookies<IServicesAliasesTable[]>("services_aliases", updated_aliases_table);
+        NapicuCookies.setCookies<IServicesAliasesTable[]>(NapicuServiceAliasManager.COOKIES_SERVICES_ALIASES_TABLE_NAME, updated_aliases_table);
     }
 
     public static get_alias_by_key(key: string): string | null {
-        return NapicuCookies.getCookies<IServicesAliasesTable[]>("services_aliases")?.find((item: IServicesAliasesTable) => {
+        return NapicuCookies.getCookies<IServicesAliasesTable[]>(NapicuServiceAliasManager.COOKIES_SERVICES_ALIASES_TABLE_NAME)?.find((item: IServicesAliasesTable) => {
             return item.name === key;
         })?.alias ?? null;
     }
 
     public static isAliasDuplicate(alias: string): boolean {
-        return NapicuCookies.getCookies<IServicesAliasesTable[]>("services_aliases")?.some(item => item.alias === alias) ?? false;
+        return NapicuCookies.getCookies<IServicesAliasesTable[]>(NapicuServiceAliasManager.COOKIES_SERVICES_ALIASES_TABLE_NAME)?.some(item => item.alias === alias) ?? false;
     }
 
     public static get_aliases(): IServicesAliasesTable[] | null {
-        return NapicuCookies.getCookies<IServicesAliasesTable[]>("services_aliases");
+        return NapicuCookies.getCookies<IServicesAliasesTable[]>(NapicuServiceAliasManager.COOKIES_SERVICES_ALIASES_TABLE_NAME);
     }
-
-
 }
